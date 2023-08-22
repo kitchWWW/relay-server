94399ed0e140de80f88b29774da38f00f8abb8be
Brian Ellis <bse@fb.com>
2023-08-22 11:07:19 +0200

    clear

--- app.py
+++ app.py
@@ -8,20 +8,31 @@ allData = {}
 
 @app.route('/post_message/<uuid>', methods=['POST'])
 def post_message(uuid):
+    print("post_message")
     print(uuid)
     allData[uuid] = request.json
     return jsonify({"success":True})
 
 @app.route('/append_message/<uuid>', methods=['POST'])
 def append_message(uuid):
+    print("append_message")
     print(uuid)
     if(uuid not in allData):
         allData[uuid] = []    
     allData[uuid].append(request.json)
     return jsonify({"success":True})
 
+@app.route('/clear_message/<uuid>', methods=['POST'])
+def clear_message(uuid):
+    print("clear_message")
+    print(uuid)
+    if(uuid in allData):
+        del allData[uuid]
+    return jsonify({"success":True})
+
 @app.route('/get_message/<uuid>', methods=['GET'])
 def get_message(uuid):
+    print("get_message")
     print(uuid)
     if(uuid in allData):
         return jsonify(allData[uuid])
